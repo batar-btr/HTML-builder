93bfbbf3cfb7bc7eea5444306c5f4d05a2b53ab6
batar-btr <btrn86@gmail.com>
2022-11-02 19:18:56 +0200

    feat: add solution to 03-files-in-folder task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,20 @@
+
+const path = require('path');
+const fs = require('fs');
+const { readdir } = require('node:fs/promises');
+
+const show = async function (pathToFile) {
+  try {
+    const files = await readdir(pathToFile, { withFileTypes: true });
+    for (const file of files)
+      if (file.isFile()) {
+        let [name, ext] = file.name.split('.');
+        fs.stat(
+          path.join(pathToFile, file.name),
+          (err, stats) => console.log(`${name} - ${ext} - ${stats.size / 8000}Kb`));
+      }
+  } catch (err) {
+    console.error(err);
+  }
+}
+show(path.join(__dirname, 'secret-folder'));
